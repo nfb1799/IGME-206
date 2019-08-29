b95ebc19678ee894dfd28b9c90163b1251f23dd1
nfb1799 <nfb1799@g.rit.edu>
2019-08-29 16:13:08 -0400

    Create Hello.cs
    Added Hello.cs file

---
+++ Hello.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My name is Nik and I play Hearthstone. It is Blizzard's card game.");
+            Console.ReadKey();
+        }
+    }
+}
