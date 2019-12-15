3430ffff2949f951e5cd62e5a4f1ca3ad7e15ce0
Nik Barbero (RIT Student) <nfb1799@ad.rit.edu>
2019-12-14 20:07:21 -0500

    Final Project
    Initial Upload

---
+++ finalProject/Assets/CubeSystem.cs
@@ -0,0 +1,37 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using static Unity.Mathematics.math;
+
+public class CubeSystem : JobComponentSystem
+{
+    public static float totalTime = 0;
+
+    [BurstCompile]
+    struct CubeSystemJob : IJobForEach<Translation, Rotation, CubeData>
+    {
+        public float time;
+
+
+
+        public void Execute(ref Translation translation, [ReadOnly] ref Rotation rotation, [ReadOnly] ref CubeData cubeData)
+        {
+            //Moves the cube up and down depending on the speed of the cube
+            float y = cubeData.speed * math.sin(PI * time);
+            translation.Value.y = y;
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+        var job = new CubeSystemJob();
+
+        totalTime += UnityEngine.Time.deltaTime;
+        job.time = totalTime;
+
+        return job.Schedule(this, inputDependencies);
+    }
+}
